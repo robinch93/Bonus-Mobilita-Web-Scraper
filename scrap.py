48223bcfb30a9a9eddcffb634310261062603c8d
robin <34096241+robinch93@users.noreply.github.com>
2021-07-10 22:57:29 +0200

    adding UI

--- scrap.py
+++ scrap.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs4
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
 from datetime import datetime
 from time import sleep
+from pathlib import Path
 import pandas as pd
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from lxml import etree
 import os
+import streamlit as st
+from geosky import geo_plug
+from translate import Translator
+import json
 
 website = """
 #########################################
@@ -16,57 +22,66 @@ website = """
 ######################################### 
 """
 print(website);
-start_time = datetime.now();
-print('Scrap starting time : {}' .format(start_time.time()));
+# start_time = datetime.now();
+# print('Scrap starting time : {}' .format(start_time.time()));
 
 store_names, store_address, website, email, phone = [],[],[],[],[] 
 
 url = "https://www.buonomobilita.it/mobilita2020/#/doveUsareBuoni"
 
 # dict = {'city' : {pages to scrap}}
-dict = {}
+# dict = {}
 
-file = open("inputCities.txt",'r')
-lines = file.readlines()
-count = 0
-for line in lines: 
-    data = line.split(',')
-    dict[data[0]]=int(data[1])
+# file = open("inputCities.txt",'r')
+# lines = file.readlines()
+# count = 0
+# for line in lines: 
+#     data = line.split(',')
+#     dict[data[0]]=int(data[1])
 
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('window-size=1980,1080')
 options.add_argument('--start-maximized')
-driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options);
+options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(),options=options);
 driver.get(url)
 action = ActionChains(driver)
 sleep(2)
 
 print('\n')
-print(dict)
+# print(dict)
     
 def fillcity(city):
+    if city in english_name_cities:
+        city = translateToItalian(city)
     cityElement = driver.find_element_by_xpath("//input[@placeholder='Provincia']")
     action.move_to_element(cityElement).perform()
     sleep(2)
     cityElement.clear()
-    cityElement.send_keys(city)
+    cityElement.send_keys(city.strip())
     cityElement.send_keys(Keys.ENTER)
 
-def scrap():
-    for city in dict:
+def scrap(city,inputPages):
+    # for city in dict:
         fillcity(city)
         sleep(2)
 
-        inputPages = dict[city]
+        # inputPages = dict[city]
+        # inputPages = 2
         storesText = driver.find_element_by_xpath("//div[@class='col-12 list_shop']").text
         
         # validate city name
         if 'Negozi fisici e online' in storesText:
             totalStores = storesText.replace('Negozi fisici e online','').replace(' ','')
-            totalPages = int(totalStores)//10
-            print('\nCity '+ city +' has total '+ totalStores + ' stores.')
+            totalStores = int(totalStores)
+            if totalStores > 10 :
+                totalPages = totalStores//10
+            elif totalStores < 10 :
+                totalPages = totalStores % 10
+            print(totalPages)
+            print('\nCity '+ city +' has total '+ str(totalStores) + ' stores.')
             
             # validate user input for number of scraping pages
             if totalPages >= inputPages:
@@ -100,41 +115,91 @@ def scrap():
                     sleep(2)
 
                 print('--- For city {}, scraping for total {} pages completed ------'.format(city,inputPages))
-                showOutput(city)
-                for list in [store_names, store_address, website, email, phone]:
-                    list.clear()
+                # showOutput(city)
+                # for list in [store_names, store_address, website, email, phone]:
+                #     list.clear()
 
             else:
                 try:
                     input = int(inputPages)
                 except ValueError:
                     print("Number of pages is not an integer")
-                print('For this city user input is: '+ str(inputPages) +' pages. Maximum pages that can be scraped is ' + str(totalPages))
-                continue
+                st.write('For this city user input is: '+ str(inputPages) +' pages. Maximum pages that can be scraped is ' + str(totalPages))
+                # continue
 
         elif 'Nessun punto vendita soddisfa i criteri di ricerca' in storesText:
-            print('\nCity '+ city +' does not exist. Please fill a valid city name')
-            continue
+            st.write('\n'+ city +' does not exist. Please select a valid city name')
+            # continue
     
-    dict.clear()
-    driver.quit()
+    # dict.clear()
+        driver.quit()
     
 def showOutput(city):
     data={'STORE NAME':store_names,'STORE ADDRESS':store_address,'WEBSITE':website,'EMAIL':email,'PHONE':phone}
     df=pd.DataFrame(data=data)
     df.index = df.index + 1
 
-    # printing single excel for each city
-    excel_name = city + '_{}.xlsx' .format(datetime.now().date())
+    Path("CSVOutput").mkdir(parents=True, exist_ok=True) 
+
+    # printing single csv for each city
     csv_name = city + '_{}.csv' .format(datetime.now().date())
-    df.to_excel(os.path.join(os.path.curdir, 'ExcelOutput/' + excel_name))
     df.to_csv(os.path.join(os.path.curdir, 'CSVOutput/' + csv_name))
 
 def listostring(list):
     string = ""
     if(len(list)==0):
         list.append('NA')
-    return (string.join(list).replace(' ','')) 
+    return (string.join(list)) 
 
-scrap()
+def getItalianCities():
+    states = geo_plug.all_Country_StateNames()
+    states = json.loads(states)
+    regions = []
+    all_cities = []
+
+    for i in range(len(states)):
+        if list(states[i].keys())[0] == "Italy":
+            regions = list(states[i].values())[0]
+            
+    for i in range(len(regions)):
+        regionalCities = geo_plug.all_State_CityNames(regions[i])
+        regionalCities = json.loads(regionalCities)
+        regionalCities = list(regionalCities[0].values())[0]
+        all_cities = all_cities + regionalCities
+
+    return sorted(all_cities)
+
+english_name_cities = ['Florence', 'Genoa', 'Rome', 'Turin', 'Venice']
+
+def translateToItalian(text):
+    translator= Translator(to_lang="it")
+    translation = translator.translate(text)
+    return translation
+
+def app():
+    
+    header = st.beta_container()
+    selection = st.beta_container()
+    results = st.beta_container()
+   
+    with header:
+        st.write("[Bonus Mobilita 2020](https://www.buonomobilita.it/mobilita2020/#/doveUsareBuoni)")
+
+    with selection:
+        with st.form('Form 1'):
+            col1, col2 = st.beta_columns((2,1))
+            city = col1.selectbox('city',(getItalianCities()))
+            pages = col2.selectbox('pages',(1,2,3,4,5))
+            submit = st.form_submit_button('Submit')
+
+    with results:
+        if submit:
+            scrap(city,pages)
+            data={'STORE NAME':store_names,'STORE ADDRESS':store_address,'WEBSITE':website,'EMAIL':email,'PHONE':phone}
+            df=pd.DataFrame(data=data)
+            # df.index = [""] * len(df)
+            st.table(df)
+
+app()
+
 
